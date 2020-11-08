87b0e5c8536b3b59f5f9b059ee2681c478897f89
r-mestre <rmestre@unal.edu.co>
2020-11-08 18:34:28 -0500

    Creacion de 2 metodos de cifrado

---
+++ Cifrado Ascii.py
@@ -0,0 +1,55 @@
+
+
+def cifrar(mensaje):
+    mensaje_cifrado = []
+    for a in mensaje:
+        b=a
+        b=str(ord(b))
+        mensaje_cifrado.append(b)
+    return ' '.join(mensaje_cifrado)
+
+
+def descifrar(mensaje):
+    crip = mensaje.split(' ')
+    mensaje_descifrado = []
+    l=len(crip); i=0
+    while i<l:
+        b=chr(int(crip[i]))
+        mensaje_descifrado.append(b)
+        i+=1
+        
+    return ' '.join(mensaje_descifrado)
+
+def run():
+
+    while True:
+
+        opcion = str(input('''Bienvenido al cifrador. ¿Qué deseas hacer? Pulsa la tecla correspondiente:
+            [c]ifrar
+            [d]escifrar
+            [s]alir
+            '''))
+
+        if opcion == 'c':
+            mensaje = str(input('Ingresa el mensaje que deseas cifrar: '))
+            mensaje_cifrado = cifrar(mensaje)
+            print(mensaje_cifrado)
+
+        elif opcion == 'd':
+            mensaje = str(input('Ingresa la oracion que deseas descifrar: '))
+            mensaje_descifrado = descifrar(mensaje)
+            print(mensaje_descifrado)
+
+        elif opcion == 's':
+            print('¡Gracias por jugar, vuelve pronto!')
+            break
+
+        else:
+            print('No has seleccionado ninguna opción valida. Ejecuta el programa nuevamente.')
+            break
+
+if __name__ == '__main__':
+    run()
+    
+    
+    
