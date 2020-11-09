96e4e4383866846616788de0f28fb0ba8e75e57d
r-mestre <rmestre@unal.edu.co>
2020-11-08 21:45:03 -0500

    Arreglo de funcion descifrar
    Ahora ya no deja espacios extra al descifrar un mensaje

--- Cifrado Ascii.py
+++ Cifrado Ascii.py
@@ -10,15 +10,17 @@ def cifrar(mensaje):
 
 
 def descifrar(mensaje):
-    crip = mensaje.split(' ')
-    mensaje_descifrado = []
-    l=len(crip); i=0
+    crip = mensaje.split()
+    
+    mensaje_descifrado = chr(int(crip[0]))
+    l=len(crip); i=1
     while i<l:
         b=chr(int(crip[i]))
-        mensaje_descifrado.append(b)
+        mensaje_descifrado=mensaje_descifrado+b
+        #mensaje_descifrado.append(b)
         i+=1
         
-    return ' '.join(mensaje_descifrado)
+    return mensaje_descifrado
 
 def run():
 
